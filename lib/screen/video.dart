1c374fe622db698e1fb0b64e10367431eb280ee3
Gongchaeyeon <dnjftjd53@naver.com>
2022-08-26 05:04:15 +0900

    Refactor: 불필요한 코드 제거

--- lib/screen/video.dart
+++ lib/screen/video.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:chopstick2/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() => runApp(VideoPlayerApp());
 
@@ -49,91 +48,79 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      // child: SizedBox(
-      child: Container(
-        child: Text("Welcome to Chopstick !"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chopsticks Video'),
+      ),
+      // VideoPlayerController가 초기화를 진행하는 동안 로딩 스피너를 보여주기 위해
+      // FutureBuilder를 사용합니다.
+      body: FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // 만약 VideoPlayerController 초기화가 끝나면, 제공된 데이터를 사용하여
+            // VideoPlayer의 종횡비를 제한하세요.
+            return Container(
+              alignment: Alignment.center,
+              width: double.infinity,
+              // height: 200, //height 설정가능
+              child: AspectRatio(
+                //여기는 영상부분---------------------------
+                aspectRatio: _controller.value.aspectRatio, //영상 비율
+                // 영상을 보여주기 위해 VideoPlayer 위젯을 사용합니다.
+                child: VideoPlayer(_controller), //--------------------------
+              ),
+            );
+          } else {
+            // 만약 VideoPlayerController가 여전히 초기화 중이라면,
+            // 로딩 스피너를 보여줍니다.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
 
-      builder: ((context, child) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Chopsticks Video'),
-          ),
-          // VideoPlayerController가 초기화를 진행하는 동안 로딩 스피너를 보여주기 위해
-          // FutureBuilder를 사용합니다.
-          body: FutureBuilder(
-            future: _initializeVideoPlayerFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                // 만약 VideoPlayerController 초기화가 끝나면, 제공된 데이터를 사용하여
-                // VideoPlayer의 종횡비를 제한하세요.
-                return Container(
-                  alignment: Alignment.center,
-                  width: double.infinity,
-                  // height: 200, //height 설정가능
-                  child: AspectRatio(
-                    //여기는 영상부분---------------------------
-                    aspectRatio: _controller.value.aspectRatio, //영상 비율
-                    // 영상을 보여주기 위해 VideoPlayer 위젯을 사용합니다.
-                    child:
-                        VideoPlayer(_controller), //--------------------------
-                  ),
+      floatingActionButton: Stack(
+        //여기가 버튼
+        children: <Widget>[
+          Align(
+            alignment: Alignment(
+                Alignment.bottomRight.x, Alignment.bottomRight.y - 0.2),
+            child: FloatingActionButton(
+              onPressed: () {
+                // 재생/일시 중지 기능을 `setState` 호출로 감쌉니다. 이렇게 함으로써 올바른 아이콘이
+                // 보여집니다.
+                setState(
+                  () {
+                    // 영상이 재생 중이라면, 일시 중지 시킵니다.
+                    if (_controller.value.isPlaying) {
+                      _controller.pause();
+                    } else {
+                      // 만약 영상이 일시 중지 상태였다면, 재생합니다.
+                      _controller.play();
+                    }
+                  },
                 );
-              } else {
-                // 만약 VideoPlayerController가 여전히 초기화 중이라면,
-                // 로딩 스피너를 보여줍니다.
-                return Center(child: CircularProgressIndicator());
-              }
-            },
-          ),
-
-          floatingActionButton: Stack(
-            //여기가 버튼
-            children: <Widget>[
-              Align(
-                alignment: Alignment(
-                    Alignment.bottomRight.x, Alignment.bottomRight.y - 0.2),
-                child: FloatingActionButton(
-                  onPressed: () {
-                    // 재생/일시 중지 기능을 `setState` 호출로 감쌉니다. 이렇게 함으로써 올바른 아이콘이
-                    // 보여집니다.
-                    setState(
-                      () {
-                        // 영상이 재생 중이라면, 일시 중지 시킵니다.
-                        if (_controller.value.isPlaying) {
-                          _controller.pause();
-                        } else {
-                          // 만약 영상이 일시 중지 상태였다면, 재생합니다.
-                          _controller.play();
-                        }
-                      },
-                    );
-                  },
-                  heroTag: 'contact',
-                  child: Icon(
-                    _controller.value.isPlaying
-                        ? Icons.pause
-                        : Icons.play_arrow,
-                  ),
-                ),
+              },
+              heroTag: 'contact',
+              child: Icon(
+                _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
               ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: FloatingActionButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => HomeScreen()));
-                  },
-                  // heroTag: 'contact',
-                  child: Icon(Icons.next_plan),
-                ),
-              ),
-            ],
+            ),
           ),
-          // 이 마지막 콤마는 build 메서드에 자동 서식이 잘 적용될 수 있도록 도와줍니다.
-        );
-      }),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: FloatingActionButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()));
+              },
+              // heroTag: 'contact',
+              child: Icon(Icons.next_plan),
+            ),
+          ),
+        ],
+      ),
+      // 이 마지막 콤마는 build 메서드에 자동 서식이 잘 적용될 수 있도록 도와줍니다.
     );
   }
 }
