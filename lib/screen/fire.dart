188ca887d9ee5f32126e67017c37470c99c9160d
Gongchaeyeon <dnjftjd53@naver.com>
2022-09-14 21:52:48 +0900

    Feat: 폭죽 기능 추가 추후에 미니게임이 끝난 후 점수와 그래프, 폭죽이 함께 실행됨

---
+++ lib/screen/fire.dart
@@ -0,0 +1,98 @@
+import 'package:confetti/confetti.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(fire());
+
+class ConfettiSample extends StatelessWidget {
+  const ConfettiSample({required Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Confetti',
+        home: Scaffold(
+          backgroundColor: Colors.grey[900],
+          body: fire(),
+        ));
+  }
+}
+
+class fire extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<fire> {
+  late ConfettiController _controllerCenter;
+
+  @override
+  void initState() {
+    _controllerCenter =
+        ConfettiController(duration: const Duration(seconds: 2));
+    super.initState();
+    _controllerCenter.play();
+  }
+
+  @override
+  void dispose() {
+    _controllerCenter.dispose();
+    super.dispose();
+  }
+
+  Path drawHeart(Size size) {
+    double width = size.width;
+    double height = size.height;
+
+    Path path = Path();
+
+    path.moveTo(0.5 * width, height * 0.35);
+    path.cubicTo(0.2 * width, height * 0.1, -0.25 * width, height * 0.6,
+        0.5 * width, height);
+    path.moveTo(0.5 * width, height * 0.35);
+    path.cubicTo(0.8 * width, height * 0.1, 1.25 * width, height * 0.6,
+        0.5 * width, height);
+
+    path.close();
+    return path;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.transparency,
+      child: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.center,
+              child: ConfettiWidget(
+                confettiController: _controllerCenter,
+                blastDirectionality: BlastDirectionality.explosive,
+                shouldLoop: true,
+                colors: const [
+                  Colors.green,
+                  Colors.blue,
+                  Colors.pink,
+                  Colors.orange,
+                  Colors.purple
+                ],
+                createParticlePath: drawHeart,
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                '수고하셨습니다\n당신의 점수는 [ ]점 입니다.',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
