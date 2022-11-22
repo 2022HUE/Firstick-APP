bc44056839bc4e8a740a4cd1712a51d757198098
anjiwon319 <anjiwon319@kookmin.ac.kr>
2022-11-22 19:57:52 +0900

    Feat: 리스트뷰 색상 변경(보라->파랑)

--- lib/screen/level_view.dart
+++ lib/screen/level_view.dart
@@ -22,100 +22,121 @@ class _LevelViewState extends State<LevelView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: ListView(children: <Widget>[
-        Stack(
-          children: <Widget>[
-            Container(
-              child: Container(
-                padding: EdgeInsets.only(top: 250),
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      height: 70,
-                      width: 500,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: Colors.purple[1 * 100],
-                      ),
-                      child: FlatButton(
-                        onPressed: () {
-                          locator<InferenceService>().setModelConfig();
-                          Navigator.push(context, MaterialPageRoute(
-                            builder: (context) {
-                              return Camera(title: "1단계");
-                            },
-                          ));
-                        },
-                        child: Text("Level 1"),
-                      ),
-                    ),
-                    Container(height: 10),
-                    Container(
-                      height: 70,
-                      width: 500,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: Colors.purple[2 * 100],
-                      ),
-                      child: FlatButton(
-                        onPressed: () {
-                          locator<InferenceService>().setModelConfig();
-                          Navigator.push(context, MaterialPageRoute(
-                            builder: (context) {
-                              return Camera(title: "2단계");
-                            },
-                          ));
-                        },
-                        child: Text("Level 2"),
-                      ),
-                    ),
-                    Container(height: 10),
-                    Container(
-                      height: 70,
-                      width: 500,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: Colors.purple[3 * 100],
-                      ),
-                      child: FlatButton(
-                        onPressed: () {
-                          locator<InferenceService>().setModelConfig();
-                          Navigator.push(context, MaterialPageRoute(
-                            builder: (context) {
-                              return Camera(title: "3단계");
-                            },
-                          ));
-                        },
-                        child: Text("Level 3"),
-                      ),
-                    ),
-                    Container(height: 10),
-                    Container(
-                      height: 70,
-                      width: 500,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: Colors.purple[4 * 100],
-                      ),
-                      child: FlatButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              PageTransition(
-                                  type: PageTransitionType.rightToLeft,
-                                  child: fire()));
-                        },
-                        child: Text("폭죽"),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 156, 181, 217),
+          title: Text("미니게임"),
         ),
-      ]),
-    ));
+        body: Center(
+          child: ListView(children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  child: Container(
+                    padding: EdgeInsets.only(top: 170),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 70,
+                          width: 500,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 192, 207, 225),
+                          ),
+                          child: FlatButton(
+                            onPressed: () {
+                              locator<InferenceService>().setModelConfig();
+                              Navigator.push(context, MaterialPageRoute(
+                                builder: (context) {
+                                  return Camera(title: "1단계");
+                                },
+                              ));
+                            },
+                            child: Text(
+                              "Level 1",
+                              style:
+                                  TextStyle(fontFamily: '롯데마트B', fontSize: 20),
+                            ),
+                          ),
+                        ),
+                        Container(height: 10),
+                        Container(
+                          height: 70,
+                          width: 500,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 156, 181, 217),
+                          ),
+                          child: FlatButton(
+                            onPressed: () {
+                              locator<InferenceService>().setModelConfig();
+                              Navigator.push(context, MaterialPageRoute(
+                                builder: (context) {
+                                  return Camera(title: "2단계");
+                                },
+                              ));
+                            },
+                            child: Text(
+                              "Level 2",
+                              style:
+                                  TextStyle(fontFamily: '롯데마트B', fontSize: 20),
+                            ),
+                          ),
+                        ),
+                        Container(height: 10),
+                        Container(
+                          height: 70,
+                          width: 500,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 113, 149, 199),
+                          ),
+                          child: FlatButton(
+                            onPressed: () {
+                              locator<InferenceService>().setModelConfig();
+                              Navigator.push(context, MaterialPageRoute(
+                                builder: (context) {
+                                  return Camera(title: "3단계");
+                                },
+                              ));
+                            },
+                            child: Text(
+                              "Level 3",
+                              style:
+                                  TextStyle(fontFamily: '롯데마트B', fontSize: 20),
+                            ),
+                          ),
+                        ),
+                        Container(height: 10),
+                        Container(
+                          height: 70,
+                          width: 500,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 79, 119, 180),
+                          ),
+                          child: FlatButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  PageTransition(
+                                      type: PageTransitionType.rightToLeft,
+                                      child: fire()));
+                            },
+                            child: Text(
+                              "폭죽",
+                              style:
+                                  TextStyle(fontFamily: '롯데마트B', fontSize: 20),
+                            ),
+                          ),
+                        ),
+                        Container(height: 10),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ]),
+        ));
   }
 }
